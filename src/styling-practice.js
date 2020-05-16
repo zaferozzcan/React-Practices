846ecbaacfbce314be3d26f2bf120f759b913ae8
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-16 15:39:09 -0400

    Add React Styling Practices

--- src/styling-practice.js
+++ src/styling-practice.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import greeting from "./styling-practice"
-//
-// var name = "Zafer";
-// var year = new Date().getFullYear()
-//
-// ReactDOM.render(
-//   <div>
-//     <p>Created by {name}</p>
-//     <p>Copyright {year} </p>
-//   </div>,
-//   document.getElementById("root")
-// )
-
-
 import React from "react";
 import ReactDOM from "react-dom";
 
