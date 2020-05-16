11dbc151bbac42243fdf6a46b5da8e78e40015a3
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-05-16 15:40:30 -0400

    Add Javascript Expressions in JSX Practices

--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import greeting from "./styling-practice"
-//
-// var name = "Zafer";
-// var year = new Date().getFullYear()
-//
-// ReactDOM.render(
-//   <div>
-//     <p>Created by {name}</p>
-//     <p>Copyright {year} </p>
-//   </div>,
-//   document.getElementById("root")
-// )
+import React from 'react';
+import ReactDOM from 'react-dom';
+import greeting from "./styling-practice"
 
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-const currentTime = new Date().getHours();
-
-
-var greeting;
-
-const customStyle = {
-  color: ""
-};
-
-if (currentTime < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Night";
-  customStyle.color = "blue";
-}
+var name = "Zafer";
+var year = new Date().getFullYear()
 
 ReactDOM.render(
-  <h1 className="heading" style={customStyle}>
-    {greeting}
-  </h1>,
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year} </p>
+  </div>,
   document.getElementById("root")
-);
+)
+
+
+
